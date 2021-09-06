71c6c30403102b6b59aa45deb4bc7072b666fce1
Lucas Neves <lucas54neves@gmail.com>
2021-09-06 02:30:32 -0300

    feat: add optimization

--- src/main.py
+++ src/main.py
@@ -24,10 +24,15 @@ class Environment(object):
                 'direction': 'oeste',
                 'state': 'limpo',
                 'visited': False
+            },
+            {
+                'direction': 'centro',
+                'state': 'limpo',
+                'visited': True
             }
         ]
 
-        for location in self.locations:
+        for index, location in enumerate(self.locations):
             """
                 Escolhe aleatoriamente o estado de cada localizacao
                     - Se for 1, sera limpo que ja foi definido anteriormente
@@ -36,9 +41,9 @@ class Environment(object):
             """
             state = random.randint(1,3)
 
-            if state == 2:
+            if state == 2 and index < 4:
                 location['state'] = 'pouco sujo'
-            elif state == 3:
+            elif state == 3 and index < 4:
                 location['state'] = 'muito sujo'
         
         """
@@ -61,7 +66,10 @@ class Environment(object):
     def next_location(self):
         positions = [index for index, element in enumerate(self.locations) if element['state'] != 'limpo' and not element['visited']]
 
-        index = random.randint(0,len(positions) - 1)
+        if len(positions) < 2:
+            index = 0
+        else:
+            index = random.randint(0,len(positions) - 1)
 
         self.agent_location = positions[index]
 
@@ -72,6 +80,9 @@ class Environment(object):
         print()
 
         while not self.all_clean():
+            self.print_environment()
+            print()
+
             sleep(2)
             self.next_location()
             print(f"O agente se moveu para o {self.locations[self.agent_location]['direction']}.")
@@ -84,11 +95,58 @@ class Environment(object):
                 print(f"O ambiente {self.locations[self.agent_location]['direction']} esta muito sujo, lavando...")
                 self.locations[self.agent_location]['state'] = 'limpo'
             sleep(2)
+
+            self.print_environment()
+            print()
+
+            sleep(2)
+
             self.agent_location = 4
             print('O agente voltou para o centro.')
             print()
+        
+        self.print_environment()
+        print()
 
         print('Tudo limpo.')
+    
+    def print_environment(self):
+        print('[X] - Agente')
+        print('[1] - Ambiente limpo')
+        print('[2] - Ambiente pouco sujo')
+        print('[3] - Ambiente muito sujo')
 
-ambiente = Environment()
-ambiente.run()
+        prints = []
+
+        for index, location in enumerate(self.locations):
+            if index == self.agent_location:
+                prints.append('X')
+            elif location['state'] == 'limpo':
+                prints.append('1')
+            elif location['state'] == 'pouco sujo':
+                prints.append('2')
+            elif location['state'] == 'muito sujo':
+                prints.append('3')
+        
+        print()
+        print(f'\t|{prints[0]}|\t')
+        print(f'|{prints[3]}|\t|{prints[4]}|\t|{prints[2]}|')
+        print(f'\t|{prints[1]}|\t')
+
+def main():
+    number_of_environments = int(input('Quantas vezes deseja executar? '))
+
+    for i in range(number_of_environments):
+        print(f'Rodada {i + 1}:')
+        print()
+
+        ambiente = Environment()
+        ambiente.run()
+
+        print()
+        print('Rodada finalizada')
+        print()
+
+        sleep(2)
+
+main()
